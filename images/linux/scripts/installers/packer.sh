f8da81b62b060f71a5511580e61c87a23e3a629d
Himesh <43146174+himesh99@users.noreply.github.com>
2024-05-07 18:18:10 +0100

    Update packer.sh

--- images/linux/scripts/installers/packer.sh
+++ images/linux/scripts/installers/packer.sh
@@ -14,5 +14,4 @@ download_with_retries "${URL}" "/tmp" "${ZIP_NAME}"
 unzip -qq "/tmp/${ZIP_NAME}" -d /usr/local/bin
 rm -f "/tmp/${ZIP_NAME}"
 
-packer plugins install github.com/hashicorp/azure
 invoke_tests "Tools" "Packer"
